d2fd6bb1541bec51485fa728c55069a7ce88ccf9
Eleos <Peaceaanu247@gmail.com>
2022-07-22 15:58:29 +0100

    updated app features to display network logos

--- script.js
+++ script.js
@@ -34,32 +34,56 @@ const nineMobilePrefixes = ["809", "818", "817", "908", "909"];
 
 function validatePhoneNumber() {
   phoneNumber.addEventListener("input", (e) => {
+    //Get the phone number entered by the user and store it in a variable
     let userPhoneNumber = e.target.value;
 
+    //Get the prefix of the phone number and store in a variable
     let userPhoneNumberPrefix = userPhoneNumber.substring(1, 4);
 
+    //Display an error message on the form if the user enters an invalid phone number
+
+    if (userPhoneNumber.length != 11) {
+      document.querySelector(".error-msg").style.display = "block";
+    } else {
+      document.querySelector(".error-msg").style.display = "";
+    }
+
+    // Get the prefix of the phone number that the user enters and check each array of network providers for a match. If it matches, display the logo of the network provider
+
     if (
-      userPhoneNumber.length == 11 &&
-      mtnPrefixes.includes(userPhoneNumberPrefix) == true
+      mtnPrefixes.includes(userPhoneNumberPrefix) &&
+      userPhoneNumber.length == 11
     ) {
       phoneNumber.classList.add("mtn");
-    } else if (
-      userPhoneNumber.length == 11 &&
-      gloPrefixes.includes(userPhoneNumberPrefix) == true
+    } else {
+      phoneNumber.classList.remove("mtn");
+    }
+
+    if (
+      gloPrefixes.includes(userPhoneNumberPrefix) &&
+      userPhoneNumber.length == 11
     ) {
       phoneNumber.classList.add("glo");
-    } else if (
-      userPhoneNumber.length == 11 &&
-      airtelPrefixes.includes(userPhoneNumberPrefix) == true
+    } else {
+      phoneNumber.classList.remove("glo");
+    }
+
+    if (
+      airtelPrefixes.includes(userPhoneNumberPrefix) &&
+      userPhoneNumber.length == 11
     ) {
       phoneNumber.classList.add("airtel");
-    } else if (
-      userPhoneNumber.length == 11 &&
-      nineMobilePrefixes.includes(userPhoneNumberPrefix) == true
+    } else {
+      phoneNumber.classList.remove("airtel");
+    }
+
+    if (
+      nineMobilePrefixes.includes(userPhoneNumberPrefix) &&
+      userPhoneNumber.length == 11
     ) {
       phoneNumber.classList.add("nine-mobile");
     } else {
-      return;
+      phoneNumber.classList.remove("nine-mobile");
     }
   });
 }
